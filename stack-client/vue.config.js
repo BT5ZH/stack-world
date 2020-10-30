166488eee1e65c74856a8c4e70162e410b198ef5
BoTingZH <540761800@.com>
2020-10-31 00:01:09 +0800

    add deploy script in travis.yml and update vue config file

--- stack-client/vue.config.js
+++ stack-client/vue.config.js
@@ -1,14 +1,16 @@
 module.exports = {
+  publicPath: "/",
+  assetsDir: "public",
   devServer: {
     disableHostCheck: true,
   },
   css: {
     loaderOptions: {
       sass: {
-        prependData: `@import "~@/assets/scss/variables.scss";`,
+        prependData: `@import "~@/assets/scss/main.scss";`,
       },
       scss: {
-        prependData: `@import "~@/assets/scss/variables.scss";`,
+        prependData: `@import "~@/assets/scss/main.scss";`,
       },
     },
   },
