bfefd8101c130d549c03f94ad475cee449a36485
henrenx <henrenx@outlook.com>
2020-11-10 15:30:12 +0800

    修改配置文件,使可运行

--- mongo/setup.sh
+++ mongo/setup.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 mongo <<EOF
 use admin;
+db.auth("root","zhanghuiquan")
 db.createUser({ user: 'btUser', pwd: 'btPass', roles: [ { role: "userAdminAnyDatabase", db: "admin" } ] });
-db.auth("btUser","btPass")
+db.grantRolesToUser("btUser",[{role:"dbOwner",db:"stack-learning"}]);
 use stack-learning;
 db.createCollection("courses");
 EOF
 
-mongoimport --db stack-learning --collection courses --file $WORKSPACE/courses.json
+# mongoimport --db stack-learning --collection courses --file $WORKSPACE/courses.json
