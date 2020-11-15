c21a3c199eb37aaefba90cc524afc843172a0207
BoTingZH <540761800@.com>
2020-11-15 10:37:06 +0800

    add organization and class api

---
+++ server/routes/classRoutes.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const classController = require("../controller/classController");
+const authController = require("../controller/authController");
+
+const router = express.Router();
+
+router
+  .route("/")
+  .get(authController.protect, classController.getAllClasses)
+  .post(authController.protect, classController.createClass);
+router
+  .route("/:id")
+  .get(authController.protect, classController.getClass)
+  .patch(authController.protect, classController.updateClass)
+  .delete(authController.protect, classController.deleteClass);
+router
+  .route("/:id/students")
+  // .get(authController.protect, classController.getStudents);
+  .patch(authController.protect, classController.updateStudents)
+  .post(authController.protect, classController.addStudents);
+// .delete(authController.protect, classController.deleteStudents);
+// router
+//   .route("/:id/student/:sid")
+//   .get(authController.protect, classController.getStudent)
+//   .patch(authController.protect, classController.updateStudent)
+//   .post(authController.protect, classController.addStudent)
+//   .delete(authController.protect, classController.deleteStudent);
+
+module.exports = router;
