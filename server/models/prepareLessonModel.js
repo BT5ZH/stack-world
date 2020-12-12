25064fced62d348eec83c06707cc5b4881cf82be
Chao Qi <chaos@ChaodeiMac.local>
2020-12-12 20:53:42 +0800

    qc 12-12 1

--- server/models/prepareLessonModel.js
+++ server/models/prepareLessonModel.js
@@ -8,58 +8,46 @@ const prepareLessonSchema = new mongoose.Schema(
       required: [true, "Please tell us the ID"],
       default: uuid.v1
     },
-    lesson_id: {
-      type: mongoose.Schema.Types.String,
-      ref: "Lesson"
-      // type:String
+    lesson_id:{
+        type: mongoose.Schema.Types.String,
+        ref: 'Lesson',
     },
     teacher_id: {
-      type: mongoose.Schema.Types.String,
-      ref: "User"
-      // type:String
-
+        type: mongoose.Schema.Types.String,
+        ref: 'User',
     },
-    one_class: [
-      {
-        name: {
-          type: String,
-          //required: [true, "One class must have a name"]
-        },
-        PPT: {
-          type: String //URL
-        },
-        duration: {
-          type: Number,
-          default: 50
-        },
-        description: {
-          type: String
-        },
-
-        nodes: [
-          {
-            tag: {
-              type: String,
-              default: "Teach",
-              enum: [
-                "Teach",
-                "Sign",
-                "Ask",
-                "Race",
-                "Vote",
-                "Dispatch",
-                "Test",
-                "Homework"
-              ]
+    one_class:[
+        {
+          name:  {
+            type:String,
+            required: [true, 'One class must have a name'],
+          },
+          PPT:{
+              type:String,//URL
             },
-            start: Number,
-            end: Number,
-            content: String,
-            options: [{ type: String }],
-            attachment_url: { type: String } //use to save the attachment url of one homework
-          }
-        ]
-      }
+          duration: {
+                type: Number,
+                default:50
+          },
+          description: {
+              type: String,
+            
+          },
+          nodes: [
+                  {
+                      tag: {
+                              type: String,
+                              default: 'Teach',
+                              enum: ['Teach', 'Sign','Ask','Race','Vote','Dispatch','Test','Homework',],
+                      },
+                      start: Number,
+                      end:Number,
+                      content:String,
+                      options:[{type:String,}],
+                      attachment_url: { type: String },//use to save the attachment url of one homework
+                  }
+          ],
+        }
     ]
   },
   { _id: false }
