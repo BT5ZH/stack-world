f1984f6a1901013ed8e327ceb38ed5fe47e5d6e0
41612196 <1185391863@qq.com>
2020-12-13 10:56:28 +0800

    12-13pwj

--- server/controller/prepareLessonController.js
+++ server/controller/prepareLessonController.js
@@ -111,7 +111,7 @@ exports.addNewSection = catchAsync(async (req, res) => {
 });
 
 /**
- * 将某一门课删除某一课时备课
+ * 将一门课的某一课时备课删除
  * req中包括课程信息如lesson_id、备课教师信息teacher_id、课时信息section_index（课时的序号从1开始）
  */
 exports.deleteSection = catchAsync(async (req, res) => {
