5763bf8de2e3550793c6e1c43bef5d48eee858f8
Nuo Cheng <873597000@qq.com>
2020-12-14 20:21:35 +0800

    2020/12/14 cn

--- server/app.js
+++ server/app.js
@@ -28,11 +28,13 @@ const classRouter = require("./routes/classRoutes");
 const deviceRouter = require("./routes/deviceRoutes");
 const resourceRouter = require("./routes/resourceRoutes");
 const activityRouter = require("./routes/liveActivityRoutes");
-const campusRouter = require("./routes/campusRoutes");
-const roomRouter = require("./routes/roomRoutes");
-const timeTableRouter = require("./routes/timeTableRoutes");
 const prepareLessonRouter = require("./routes/prepareRoutes");
 
+// Nuo
+const campusRouter = require("./routes/campusRoutes");
+const buildingRouter = require("./routes/buildingRouter");
+const roomRouter = require("./routes/roomRoutes");
+const timeTableRouter = require("./routes/timeTableRoutes");
 
 app.use("/pc/v1/courses", courseRouter);
 app.use("/pc/v1/users", userRouter);
@@ -41,10 +43,13 @@ app.use("/pc/v1/classes", classRouter);
 app.use("/pc/v1/devices", deviceRouter);
 app.use("/pc/v1/resources", resourceRouter);
 app.use("/pc/v1/activities", activityRouter);
+app.use("/pc/v1/prepare", prepareLessonRouter);
+
+//Nuo
 app.use("/pc/v1/campus", campusRouter);
+app.use("/pc/v1/building", buildingRouter);
 app.use("/pc/v1/rooms", roomRouter);
 app.use("/pc/v1/timetable", timeTableRouter);
-app.use("/pc/v1/prepare", prepareLessonRouter);
 
 app.all("*", (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
