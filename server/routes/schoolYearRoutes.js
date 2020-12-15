9166122fb06004cd02b2ee37f5ee02dc68eb88f3
41612196 <1185391863@qq.com>
2020-12-15 22:11:49 +0800

    12-15pwj

--- server/routes/schoolYearRoutes.js
+++ server/routes/schoolYearRoutes.js
@@ -4,9 +4,9 @@ const schoolYearController = require("../controller/schoolYearController");
 const router = express.Router({ mergeParams: true });
 
 router.route("/getAllSchoolYear").get(schoolYearController.getAllSchoolYear);
-router.route("/addSchoolYear").get(schoolYearController.addSchoolYear);
+router.route("/addSchoolYear").post(schoolYearController.addSchoolYear);
 router.route("/deleteSchoolYear").delete(schoolYearController.deleteSchoolYear);
-router.route("/updateSchoolYear").delete(schoolYearController.updateSchoolYear);
+router.route("/updateSchoolYear").post(schoolYearController.updateSchoolYear);
 
 router.all("*", (req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
