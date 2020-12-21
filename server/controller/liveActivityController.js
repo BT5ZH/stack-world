092680119ed5dabe87b2f9a34e35dcc55966fc3e
BoTingZH <540761800@.com>
2020-12-21 18:03:10 +0800

    活动有则更新无则创建

--- server/controller/liveActivityController.js
+++ server/controller/liveActivityController.js
@@ -5,23 +5,42 @@ const { redisClient } = require("../dbsSetup");
 const AppError = require("./../utils/appError");
 
 exports.createActivity = catchAsync(async (req, res, next) => {
+  // BUILD QUERY
+  // 1) Filtering
+  const queryObj = { ...req.query };
+  const excludedFields = ["page", "sort", "limit", "fields"];
+  excludedFields.forEach((el) => delete queryObj[el]);
+
+  // 2) Advanced filtering
+  let queryString = JSON.stringify(queryObj);
+  queryString = queryString.replace(
+    /\b(gte|gt|lte|le)\b/g,
+    (match) => `$${match}`
+  );
+
   const payload = req.body;
   console.log(payload);
   const lessonId = payload.activityID;
   const lessonNumber = payload.activityNumber;
   const teacherName = payload.teacher;
 
-  // redisClient.on("connect", function () {
-  //   console.log("redis 连接上了");
   redisClient.hset(lessonId, lessonNumber, teacherName, redis.print);
   redisClient.hget(lessonId, lessonNumber, redis.print);
   console.log("connect");
-  // });
 
-  const newActivity = await Activity.create(req.body);
+  // let query = { /* query */ };
+  // let update = {expire: new Date()};
+  let options = { upsert: true, new: true, setDefaultsOnInsert: true };
+  let data = await Activity.findOneAndUpdate(
+    JSON.parse(queryString),
+    payload,
+    options
+  );
 
-  res.status(201).json({
+  // const newActivity = await Activity.create(req.body);
+
+  res.status(200).json({
     status: "success",
-    data: newActivity,
+    data,
   });
 });
