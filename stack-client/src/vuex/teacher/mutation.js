b74da0655b1c51dd28e20209a038db12da29e110
henrenx <henrenx@outlook.com>
2020-12-21 21:03:59 +0800

    添加 socket

--- stack-client/src/vuex/teacher/mutation.js
+++ stack-client/src/vuex/teacher/mutation.js
@@ -1,3 +1,28 @@
-const mutation = {};
+const mutation = {
+  updateCourseCalendar(state, params) {
+    state.courseCalendar = params;
+  },
+  updateToday(state, params) {
+    state.today = params;
+  },
+  clearStorage(state) {
+    Object.keys(state).forEach((key) => {
+      if (Array.isArray(state[key])) {
+        state[key] = [];
+      } else if (typeof state[key] === "object") {
+        state[key] = {};
+      } else if (typeof state[key] === "number") {
+        state[key] = 0;
+      } else if (typeof state[key] === "boolean") {
+        state[key] = false;
+      } else if (typeof state[key] === "string") {
+        state[key] = "";
+      }
+    });
+  },
+  updateSignResult(state, params) {
+    state.signList.push(params);
+  }
+};
 
 export default mutation;
--- stack-client/src/vuex/teacher/state.js
+++ stack-client/src/vuex/teacher/state.js
@@ -1,3 +1,8 @@
-const state = {};
+const state = {
+  courseCalendar: [],
+  today: { title: "", day: "" },
+  weekTable: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
+  signList: [],
+};
 
 export default state;
