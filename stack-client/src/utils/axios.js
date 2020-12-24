0df3e6a7387f5fc68d744008ec2e5bd4db8e7ac3
BoTingZH <540761800@.com>
2020-12-24 13:35:06 +0800

    生产环境URL

--- stack-client/src/utils/axios.js
+++ stack-client/src/utils/axios.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 const token = localStorage.getItem("tk");
+const devURL = "http://localhost:3050/api";
+const proURL =
+  "http://stacksdocker-env-ysbhkejxhp.cn-northwest-1.eb.amazonaws.com.cn/api";
 export default axios.create({
-  baseURL: "http://localhost:3050/api",
+  baseURL: proURL,
   headers: { Authorization: "Bearer " + token },
   timeout: 5000,
 });
