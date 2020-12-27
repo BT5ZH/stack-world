47d87f177598f4729fbedfaa7daffacc7179818e
BoTingZH <540761800@.com>
2020-12-28 01:01:47 +0800

    添加教学空间标签页

--- stack-client/src/components/admin/teaching/space/Space.vue
+++ stack-client/src/components/admin/teaching/space/Space.vue
@@ -1,44 +1,81 @@
 <template>
-  <a-row class="container">
-    <a-row :span="20" style="margin: 10px 25px 20px 5px;">
-      <a-tree-select
-        style="width: 100%"
-        :value="value"
-        :dropdown-style="{ maxHeight: '400px', overflow: 'auto' }"
-        :placeholder="orgName"
-        allow-clear
-        tree-default-expand-all
-        @change="onChange"
-        @search="onSearch"
-        @select="onSelect"
-      >
-        <a-tree-select-node
-          :key="campus.campus_name"
-          :value="campus.campus_name"
-          :title="campus.campus_name"
-          v-for="campus in campusList"
+  <div>
+    <a-row class="container">
+      <a-row :span="20" style="margin: 10px 25px 20px 5px;">
+        <a-tree-select
+          style="width: 100%"
+          :value="value"
+          :dropdown-style="{ maxHeight: '400px', overflow: 'auto' }"
+          :placeholder="orgName"
+          allow-clear
+          tree-default-expand-all
+          @change="onChange"
+          @search="onSearch"
+          @select="onSelect"
         >
           <a-tree-select-node
-            :key="buildings.name"
-            :value="`${buildings._id}:${buildings.name}`"
-            :title="buildings.name"
-            v-for="buildings in campus.buildings"
+            :key="campus.campus_name"
+            :value="`${campus.campus_name}#`"
+            :title="campus.campus_name"
+            v-for="campus in campusList"
           >
-            <!-- <a-tree-select-node key="random" value="leaf1" title="my leaf" />
-            <a-tree-select-node key="random1" value="leaf2" title="your leaf" /> -->
+            <a-tree-select-node
+              :key="buildings.name"
+              :value="`${buildings.name}:${campus.campus_name}`"
+              :title="buildings.name"
+              v-for="buildings in campus.buildings"
+            >
+            </a-tree-select-node>
           </a-tree-select-node>
-        </a-tree-select-node>
-      </a-tree-select>
+        </a-tree-select>
+      </a-row>
+      <a-row>
+        <a-col> </a-col>
+
+        <a-col>
+          <a-button type="primary" @click="bulkImport_visible = true"
+            >批量添加教室</a-button
+          >
+          <!-- <a-button type="primary">添加教室</a-button> -->
+          <a-button type="primary" @click="showDeleteConfirm(checkedList)"
+            >批量删除</a-button
+          >
+        </a-col>
+      </a-row>
+      <a-row :span="20">
+        <a-tabs :active-key="activeIndex" @change="callback">
+          <a-tab-pane key="1" tab="统计信息">
+            Content of Tab Pane 1
+          </a-tab-pane>
+          <a-tab-pane key="2" tab="建筑房间列表" force-render>
+            <space-card
+              class="class-card"
+              :rooms="roomList"
+              :buildingName="currentBuilding"
+            ></space-card>
+          </a-tab-pane>
+        </a-tabs>
+      </a-row>
     </a-row>
 
-    <a-row :span="20">
-      <space-card
-        class="class-card"
-        :rooms="roomList"
-        :buildingName="currentBuilding"
-      ></space-card>
-    </a-row>
-  </a-row>
+    <a-modal
+      v-model="bulkImport_visible"
+      title="批量导入"
+      @ok="bulkimportSubmit"
+    >
+      <a-upload
+        name="file"
+        :multiple="true"
+        :action="upload_url"
+        @change="handleChange"
+      >
+        <a-button type="primary"> <a-icon type="upload" /> 上传文件 </a-button>
+      </a-upload>
+      <br />
+      <br />
+      <a-button @click="download">下载模板</a-button>
+    </a-modal>
+  </div>
 </template>
 
 <script>
@@ -53,7 +90,20 @@ export default {
       campusList: [],
       roomList: [],
       currentBuilding: "",
-      // leaf: false,
+      upload_url: "",
+      bulkImport_visible: false,
+      colors: [
+        "#9FE6B8",
+        "#FFDb5C",
+        "#FF9F7F",
+        "#F87293",
+        "#8378EA",
+        "#E7bCF3",
+        "#96BFFF",
+      ],
+      campusName: "",
+      activeIndex: "1",
+      flag: "",
     };
   },
   computed: {
@@ -66,30 +116,33 @@ export default {
   methods: {
     async onChange(data) {
       console.log("onchange:   " + data);
-      const value = data.split(":")[0];
-      console.log("onchange:   " + value);
-      this.currentBuilding = data.split(":")[1];
-      console.log("onchange:   " + this.currentBuilding);
-      const queryObject = {
-        building: value,
-      };
-      let queryString = "";
-      Object.keys(queryObject).forEach((key) => {
-        queryString += key + "=" + queryObject[key] + "&";
-      });
-      queryString = "?" + queryString.slice(0, -1);
-      const url = "/pc/v1/rooms" + queryString;
+      this.activeIndex = "2";
+      this.campusName = data;
+      this.flag = data;
+      this.callback("2");
 
-      try {
-        const { data } = await axiosInstance.get(url);
-        console.log(data);
-        this.roomList = data.rooms;
-        console.log(this.roomList);
-      } catch (err) {
-        console.log(err);
-      }
+      // const value = data.split(":")[0];
+      // this.currentBuilding = data.split(":")[1];
+      // const queryObject = {
+      //   building: value,
+      // };
+      // let queryString = "";
+      // Object.keys(queryObject).forEach((key) => {
+      //   queryString += key + "=" + queryObject[key] + "&";
+      // });
+      // queryString = "?" + queryString.slice(0, -1);
+      // const url = "/pc/v1/rooms" + queryString;
 
-      this.value = this.currentBuilding;
+      // try {
+      //   const { data } = await axiosInstance.get(url);
+      //   console.log(data);
+      //   this.roomList = data.rooms;
+      //   console.log(this.roomList);
+      // } catch (err) {
+      //   console.log(err);
+      // }
+
+      this.value = this.data;
     },
     onSearch() {
       console.log(...arguments);
@@ -109,6 +162,67 @@ export default {
         console.log(err);
       }
     },
+    callback(key) {
+      console.log(key);
+      if (key == "1") {
+        this.activeIndex = "1";
+      } else if (key == "2") {
+        console.log("3333");
+        let payload = {};
+        this.activeIndex = "2";
+        if (this.flag.slice(-1) == "#") {
+          console.log("****");
+          let temp = this.flag.slice(0, -1);
+          payload = { subOrg_name: temp };
+          // this.getCoursesFromCondition(payload);
+        } else {
+          console.log("1111");
+          let dataArray = this.flag.split(":");
+          payload = { subOrg_name: dataArray[1], major_name: dataArray[0] };
+          // this.getCoursesFromCondition(payload);
+        }
+      }
+    },
+
+    showDeleteConfirm(deleteList) {
+      console.log(deleteList);
+      deleteList.length == 0
+        ? this.$message.info("请选中要删除的项")
+        : this.$confirm({
+            title: "确认删除吗",
+            content: "数据删除后不可恢复",
+            okText: "确定",
+            okType: "danger",
+            cancelText: "取消",
+            onOk() {
+              console.log(deleteList);
+              //post deleteList
+            },
+            onCancel() {
+              console.log("Cancel");
+            },
+          });
+    },
+
+    //bulk import
+    download() {
+      let url = ""; //输入模板下载url
+      window.open(url);
+    },
+    //上传批量导入表格
+    handleChange(info) {
+      this.upload_url = ""; //上传地址
+      if (info.file.status !== "uploading") {
+        console.log(info.file, info.fileList);
+      }
+      if (info.file.status === "done") {
+        this.$message.success(`${info.file.name} 上传成功`);
+      } else if (info.file.status === "error") {
+        this.$message.error(`${info.file.name} 上传失败.`);
+      }
+    },
+    //点击上传文件
+    bulkimportSubmit() {},
   },
   mounted() {
     this.spaceList();
