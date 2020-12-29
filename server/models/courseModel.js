27383218039a7c852a12960b735a043cb404df17
Chao Qi <chaos@Mac-mini.local>
2020-12-29 20:28:34 +0800

    qc remove semesters field

--- server/models/courseModel.js
+++ server/models/courseModel.js
@@ -39,13 +39,16 @@ var courseSchema = mongoose.Schema(
     },
     semester: {
       type: Number,
-    },
-    semesters: [{
-      type: Number,
       required: [true, "course credit is required"],
       min: 1,
       max: 8,
-    }],
+    },
+    // semesters: [{
+    //   type: Number,
+    //   required: [true, "course credit is required"],
+    //   min: 1,
+    //   max: 8,
+    // }],
     weekly_hrs: {
       type: Number,
       required: [true, "course weekly_hrs is required"],
