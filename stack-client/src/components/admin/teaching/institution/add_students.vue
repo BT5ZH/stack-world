095cdafa5b931742c89eb67282d9b77ba07c372f
gongheng <gongheng@qq.com>
2021-01-03 19:28:01 +0800

    优化院系班级刷新

--- stack-client/src/components/admin/teaching/institution/add_students.vue
+++ stack-client/src/components/admin/teaching/institution/add_students.vue
@@ -16,7 +16,7 @@
       </span>
     </div>
     <a-table
-      rowKey="user_id"
+      rowKey="_id"
       :row-selection="{
         selectedRowKeys: selectedRowKeys,
         onChange: onSelectChange,
@@ -45,10 +45,12 @@ const data = [];
 export default {
   data() {
     return {
+      child_refresh: 1,
       data,
       columns,
       selectedRowKeys: [], // Check here to configure the default column
       loading: false,
+      users: ["1"],
     };
   },
   computed: {
@@ -59,34 +61,36 @@ export default {
   mounted() {
     this.getStudents();
   },
+  watch: {
+    child_refresh(val) {
+      this.getStudents();
+    },
+  },
   methods: {
     // 添加学生
     add_students() {
       // console.log("come")
-      let students = [];
-      this.selectedRowKeys.map((item) => {
-        // console.log(item)
-        console.log("----_id------");
-        console.log(this.data);
-        // students.push(this.data[item]._id);
-      });
-      // console.log(students);
-      // this.loading = true;
+      // console.log("---data----")
+      // console.log(this.data);
       setTimeout(() => {
         // 开始
         const url = "/pc/v1/classes/updateStudents";
         const requestdata = {
           class_id: this.$route.query.classId,
-          students: students,
+          students: this.selectedRowKeys,
         };
-        console.log(requestdata);
+        // console.log(requestdata)
         axiosInstance
           .patch(url, requestdata)
           .then((res) => {
-            console.log(res);
+            // console.log(res);
+            this.$message.info("添加成功");
+            this.child_refresh += 1;
+            this.$emit('refresh',this.child_refresh)
           })
           .catch((err) => {
             console.log(err);
+            this.$message.error("添加失败");
           });
         // 结束
         this.loading = false;
@@ -108,8 +112,14 @@ export default {
       const url = "/pc/v1/classes/getStudentsNotInOneClass/" + queryString;
       try {
         const { data } = await axiosInstance.get(url);
-        // console.log(data.result);
+        console.log("-----students_data------");
+        console.log(data.result);
         this.data = data.result;
+        // this.users = this.data.map(item=> {
+        //   return item.user_id
+        // })
+        // console.log("----users----")
+        // console.log(this.users)
       } catch (err) {
         console.log(err);
       }
