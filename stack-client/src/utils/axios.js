4960763dc98c29f79f0a078bc77268ec298d4277
Anna <1248725680@qq.com>
2021-01-04 16:02:45 +0800

    备课暂存到本地（除文件下发，随堂测试，ppt上传）

--- stack-client/src/utils/axios.js
+++ stack-client/src/utils/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 const token = localStorage.getItem("tk");
-const devURL = "http://localhost:3050/api";
+// const devURL = "http://localhost:3050/api";
+const devURL = "http://localhost:5000";
 const proURL =
   "http://stacksdocker-env-ysbhkejxhp.cn-northwest-1.eb.amazonaws.com.cn/api";
 export default axios.create({
