eb0a27f618609565c8f5d6d6fd68ff6c694116a7
BoTingZH <540761800@.com>
2021-01-07 19:15:11 +0800

    https

--- stack-client/src/utils/axios.js
+++ stack-client/src/utils/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 const token = localStorage.getItem("tk");
 const devURL = "http://localhost:3050/api";
 const proURL =
-  "http://stacksdocker-env-ysbhkejxhp.cn-northwest-1.eb.amazonaws.com.cn/api";
+  "https://stacksdocker-env-ysbhkejxhp.cn-northwest-1.eb.amazonaws.com.cn/api";
 export default axios.create({
   baseURL: proURL,
   headers: { Authorization: "Bearer " + token },
