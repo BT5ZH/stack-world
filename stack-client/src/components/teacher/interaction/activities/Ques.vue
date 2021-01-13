d05ab94938aca4de0ab630de5a06dcd5566e320d
gongheng <754097983@qq.com>
2021-01-13 10:28:15 +0800

    提问hu互动的修改

--- stack-client/src/components/teacher/interaction/activities/Ques.vue
+++ stack-client/src/components/teacher/interaction/activities/Ques.vue
@@ -1,11 +1,17 @@
 <template>
-  <div>
-    <a-list item-layout="horizontal">
-      <a-list-item>
-        {{ask_answer.student}}:{{ask_answer.value}}
-      </a-list-item>
-    </a-list>
-  </div>
+  <a-row>
+    <a-col :span="18" :push="3">
+      <h1 class="sign-title">提问结果</h1>
+      <a-list size="small" bordered v-if="ask_answer.value != null">
+        <a-list-item>
+          <span class="name"> {{ ask_answer.student }} </span>
+          <a-tag color="#2db7f5">{{ ask_answer.value }}</a-tag>
+        </a-list-item>
+      </a-list>
+
+      <a-empty v-else description="暂无提交结果"> </a-empty>
+    </a-col>
+  </a-row>
 </template>
 
 <script>
@@ -25,4 +31,10 @@ export default {
 </script>
 
 <style scoped>
+.sign-title {
+  font-weight: bold;
+  color: #bbb;
+  text-align: center;
+  padding: 20px 0;
+}
 </style>
