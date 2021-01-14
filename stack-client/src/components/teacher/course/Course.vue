503aa706c200a6455a64c02a6236e8f4d6073ed4
qichao791 <qichao@snnu.edu.cn>
2021-01-14 15:19:41 +0800

    update course.vue

--- stack-client/src/components/teacher/course/Course.vue
+++ stack-client/src/components/teacher/course/Course.vue
@@ -93,7 +93,7 @@ export default {
   },
   filters: {
     progressFilter({ prepareNumber, total_study_hours }) {
-      return `${prepareNumber}备课/${total_study_hours}课时`;
+      return `已备${prepareNumber}课时/共${total_study_hours}课时`;
     },
   },
   mounted() {
