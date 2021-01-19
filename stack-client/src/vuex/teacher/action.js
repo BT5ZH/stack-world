0604e185a8af1b587bcbaf87b4c5619b2cb5581c
BoTingZH <540761800@.com>
2021-01-19 13:24:59 +0800

    teacher action js

--- stack-client/src/vuex/teacher/action.js
+++ stack-client/src/vuex/teacher/action.js
@@ -37,14 +37,9 @@ const action = {
       const url = "pc/v1/lessons/getLessonsByTeacherID";
       const requestData = { teacher_id };
       const { data } = await axios.post(url, requestData);
-<<<<<<< HEAD
-      console.log(data);
-      commit("updateTeacherCourses", data.fullInfo);
-=======
       console.log("data.abstractInfo-----");
       console.log(data.abstractInfo);
       commit("updateTeacherCourses", data.abstractInfo);
->>>>>>> e7faa4148356003e82f9fed526b3e4bfc219d38d
     } catch (error) {
       console.error(error);
     }
