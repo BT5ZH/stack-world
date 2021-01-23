71609aaa4a47d43257c286a1b5782cd0050e53a8
qichao791 <70956421@qq.com>
2021-01-23 11:52:10 +0800

    qc

--- server/controller/timeTableController.js
+++ server/controller/timeTableController.js
@@ -438,7 +438,7 @@ function weeks(time1,time2) {
   for(let i=0;i<time.length;i++){
     let oneWeek =[]
     if(time[i].Mon){
-        let MonString = time[i].Mon.toISOString().substring(0,10)//time[i].Mon.getUTCFullYear()+"-"+time[i].Mon.getUTCMonth()+1+"-"+time[i].Mon.getUTCDate()
+        let MonString = time[i].Mon.toISOString().substring(0,10)
         oneWeek.push(MonString)
     }
     if(time[i].Tues){
@@ -537,7 +537,7 @@ exports.getLatestTimeTableofTeacher = catchAsync(async (req, res, next) => {
       .populate("curriculum.class_id", "class_name -_id")
       .populate("curriculum.room_id", "room_number building_name -_id");
   
-    console.log(data[0].curriculum[0])
+    //console.log(data[0].curriculum[0])
     if (!data || data.length === 0) {
       return next(new AppError("该课表不存在", 404));
     }
