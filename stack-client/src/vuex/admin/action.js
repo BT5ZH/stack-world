fb69e23db817932062fb8968ce84cca25b898ec4
Nuo Cheng <873597000@qq.com>
2021-01-27 17:09:35 +0800

    add get tree data

--- stack-client/src/vuex/admin/action.js
+++ stack-client/src/vuex/admin/action.js
@@ -1,41 +1,79 @@
 import axios from "@/utils/axios";
-const action = {
-    async change_spin_status({commit},status){
-        commit("change_spin_status",status)
-    },
-    async change_Tree_spin_status({commit},status) {
-        // console.log(status)
-        commit("change_Tree_spin_status",status)
-    },
-    getResourcesOfOneCollege({ commit }, { org_name, subOrg_name }) {
-        return new Promise((resolve,reject)=>{
-            const requestData = { org_name, subOrg_name};
-            const url = "/pc/v1/resources/getLessonResourceOfSubOrg";
-            axios.post(url, requestData)
-            .then((res ) => {
-                console.log(res)
-                resolve( res )
-              })
-              .catch((err) => {
-                reject('error-----')
-              });
-        })
-       
-    },
-    deleteResourceById({commit},resource_id){
-        return new Promise((resolve,reject)=>{
-            const url = "/pc/v1/resources";
 
-            axios.delete(`${url}/${resource_id}`)
-            .then(( ) => {
-                resolve( "success")
-              })
-              .catch((err) => {
-                reject('error-----')
-              })
+const action = {
+  async change_spin_status({ commit }, status) {
+    commit("change_spin_status", status);
+  },
+  async change_Tree_spin_status({ commit }, status) {
+    // console.log(status)
+    commit("change_Tree_spin_status", status);
+  },
+  getResourcesOfOneCollege({ commit }, { org_name, subOrg_name }) {
+    return new Promise((resolve, reject) => {
+      const requestData = { org_name, subOrg_name };
+      const url = "/pc/v1/resources/getLessonResourceOfSubOrg";
+      axios
+        .post(url, requestData)
+        .then((res) => {
+          console.log(res);
+          resolve(res);
         })
-    }
-   
+        .catch((err) => {
+          reject("error-----");
+        });
+    });
+  },
+  deleteResourceById({ commit }, resource_id) {
+    return new Promise((resolve, reject) => {
+      const url = "/pc/v1/resources";
+
+      axios
+        .delete(`${url}/${resource_id}`)
+        .then(() => {
+          resolve("success");
+        })
+        .catch((err) => {
+          reject("error-----");
+        });
+    });
+  },
+  getTreeByURLwithSpin({ commit }, url) {
+    // 显示loading
+    commit("change_spin_status", true);
+
+    return new Promise((resolve, reject) => {
+      axios
+        .get(url)
+        .then((response) => {
+          // console.log("result:");
+          // console.log(response);
+          resolve(response);
+          commit("change_spin_status", false);
+        })
+        .catch((err) => {
+          commit("change_spin_status", false);
+
+          reject(err);
+        });
+    });
+  },
+
+  getTreeByURL({ commit }, url) {
+    // 显示loading
+
+    return new Promise((resolve, reject) => {
+      axios
+        .get(url)
+        .then((response) => {
+          // console.log("result:");
+          // console.log(response);
+          resolve(response);
+        })
+        .catch((err) => {
+          reject(err);
+        });
+    });
+  },
 };
 
 export default action;
